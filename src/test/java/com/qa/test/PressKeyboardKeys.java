5ed931308dc47c1681be9c20811c3cbb2be0c294
Your Name <gaddamprathima96@gmail.com>
2018-10-23 18:31:39 +0530

    Master commit

--- src/test/java/com/qa/test/PressKeyboardKeys.java
+++ src/test/java/com/qa/test/PressKeyboardKeys.java
@@ -5,9 +5,8 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -18,16 +17,22 @@ public class PressKeyboardKeys {
 	public void Before() {
 		System.setProperty("webdriver.chrome.driver", "..//SeleniumPractice//Driver//chromedriver");
 		driver = new ChromeDriver();
+		driver.get("https://learn.letskodeit.com/p/practice");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(2000, TimeUnit.SECONDS);
 
 	}
 	@Test
-	public void test() {
+	public void test() throws Exception {
 		//Keyboard keyboard = ((HasInputDevices) driver).getKeyboard();
 		//WebElement search=driver.findElement(By.id("opentab"));
-		driver.findElement(By.xpath("ele_path")).sendKeys(Keys.ENTER);
+		driver.findElement(By.id("opentab")).sendKeys(Keys.ENTER);
+		Thread.sleep(4000);
 		
+	}
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
 	}
 
 }
